c47dee003cff5dc267670483c61f8c3e69e78332
purpleratchet <ibeater01@gmail.com>
2023-01-09 13:39:41 +0300

    not init 4

---
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -0,0 +1,10 @@
+package com.example.playlistmaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
