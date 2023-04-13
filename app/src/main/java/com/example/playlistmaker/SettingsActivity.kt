84f52c21cc51d183132e170c8e740fd5381790e3
purpleratchet <ibeater01@gmail.com>
2023-04-13 16:32:41 +0300

    not init 8

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,20 +1,49 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val sharedPrefs = getSharedPreferences("prefs", Context.MODE_PRIVATE)
+        val isDarkMode = sharedPrefs.getBoolean("isDarkMode", false)
+        if (isDarkMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+
         val backImage = findViewById<ImageView>(R.id.backButton)
         backImage.setOnClickListener {
             finish()
         }
+        val switcher = findViewById<com.google.android.material.switchmaterial.SwitchMaterial>(R.id.switcher)
+        switcher.setOnCheckedChangeListener { _, isChecked ->
+            // установка темы в зависимости от состояния Switchmaterial
+            val sharedPrefs = getSharedPreferences("prefs", Context.MODE_PRIVATE)
+            val editor = sharedPrefs.edit()
+            editor.putBoolean("isDarkMode", isChecked)
+            editor.apply()
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+            // перезагрузка активности, чтобы применить новую тему
+            val intent = Intent(this, this.javaClass)
+            finish()
+            startActivity(intent)
+        }
+
         val shareApp = findViewById<TextView>(R.id.textView3)
         shareApp.setOnClickListener {
             val sendIntent = Intent(Intent.ACTION_SEND)
